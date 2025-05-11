6e427bd3be2dfddcc9d934be1df970b71fa0f139
Bhupendra Shah <shahbhupendra9211@gmail.com>
2025-05-11 16:35:01 +0545

    adding nav and footer

--- addons/theme_yourhome/__manifest__.py
+++ addons/theme_yourhome/__manifest__.py
@@ -5,12 +5,21 @@
     'name': 'YourHome Theme',
     'description': 'YourHome website theme',
     'category': 'Theme',
-    'sequence': 10,
+    'sequence': -1,
     'version': '1.0',
     'depends': ['website'],
     'data': [
-        'views/custom_header.xml'
+        'views/custom_header.xml',
+        'views/footer.xml'
     ],
+    'assets': {
+        'web.assets_frontend': [
+            'theme_yourhome/static/src/scss/styles.scss',
+        ],
+        'web._assets_primary_variables': [
+            'theme_yourhome/static/src/scss/primary_variables.scss',
+   ],
+},
     'images': [
     ],
     'license': 'LGPL-3',
