be9f7ec827af855257fd11671393fe8c0f1e90e5
Bhupendra Shah <shahbhupendra9211@gmail.com>
2025-05-11 10:09:54 +0545

    initialize theme_yourhome

---
+++ addons/theme_yourhome/__manifest__.py
@@ -0,0 +1,17 @@
+# -*- encoding: utf-8 -*-
+# Part of Odoo. See LICENSE file for full copyright and licensing details.
+
+{
+    'name': 'YourHome Theme',
+    'description': 'YourHome website theme',
+    'category': 'Theme',
+    'sequence': 10,
+    'version': '1.0',
+    'depends': ['website'],
+    'data': [
+        'views/custom_header.xml'
+    ],
+    'images': [
+    ],
+    'license': 'LGPL-3',
+}
