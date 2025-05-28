726badb0a601199e2aacaef49e067be6a5eccec1
Bhupendra Shah <shahbhupendra9211@gmail.com>
2025-05-28 17:50:04 +0545

    added new slider

--- addons/custom_slider/__manifest__.py
+++ addons/custom_slider/__manifest__.py
@@ -16,6 +16,8 @@
     'data': [
         # 'views/custom_slider_template.xml',
         'views/snippets/custom_slider.xml',
+        'views/snippets/starbucks_slider.xml',
+        'views/snippets/bootstrap_slider.xml',
         'views/snippets/image_slider.xml',
         'views/snippets/snippets.xml',
     ],
@@ -23,6 +25,7 @@
         'web.assets_frontend': [
             # 'custom_slider/static/src/scss/custom_slider_template.css',
             'custom_slider/static/src/js/custom_slider_template.js',
+            'custom_slider/static/src/js/starbucks.js',
             'custom_slider/static/src/js/image_slider.js',
             # 'custom_slider/static/src/js/custom_slider.js',
 
@@ -31,6 +34,8 @@
             # 'custom_slider/static/src/scss/index.min.css',
             'custom_slider/static/src/scss/custom_slider.css',
             'custom_slider/static/src/scss/image_slider.scss',
+            'custom_slider/static/src/scss/starbucks.scss',
+            'custom_slider/static/src/scss/bootstrap_slider.scss',
             # 'custom_slider/static/src/scss/custom_slider.scss',
 
 
